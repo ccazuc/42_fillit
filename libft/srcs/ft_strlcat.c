607108551a14aee020f6c9fd0d3d8dd923dc1002
Clement CAZUC <ccazuc@e2r13p2.42.fr>
2017-11-11 14:16:41 +0100

    Added libft

---
+++ libft/srcs/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/07 06:04:48 by ccazuc            #+#    #+#             */
+/*   Updated: 2017/11/08 06:24:27 by ccazuc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	unsigned int	ret;
+	size_t			i;
+	size_t			j;
+
+	i = 0;
+	while (dst[i])
+		i++;
+	j = 0;
+	while (src[j])
+		j++;
+	ret = j + (size < i ? size : i);
+	j = 0;
+	while (src[j] && i < size - 1)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (ret);
+}
